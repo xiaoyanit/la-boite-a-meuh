66878ebeaaa60fafbadd70148040930dfec109b8
kevinmcdonagh <kevin@appletv.co.uk>
2009-04-17 20:33:51 +0100

    Added the proper strings to the layout.

--- src/com/novoda/meuh/Cow.java
+++ src/com/novoda/meuh/Cow.java
@@ -1,12 +1,11 @@
 package com.novoda.meuh;
 
 import android.app.Activity;
-import android.hardware.SensorListener;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class Cow extends Activity implements SensorListener {
+public class Cow extends Activity {
 	
 	private static final String	TAG	= "[moo]:";
 	private TextView	status;
@@ -18,13 +17,11 @@ public class Cow extends Activity implements SensorListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		status = (TextView) findViewById(R.id.str_status);
-		accuracy_str = (TextView) findViewById(R.id.str_accuracy);
-		coOrds = (TextView) findViewById(R.id.str_coords);
-
+		status = (TextView) findViewById(R.id.txt_status);
+		accuracy_str = (TextView) findViewById(R.id.txt_accuracy);
+		coOrds = (TextView) findViewById(R.id.txt_coords);
 	}
 
-	@Override
 	public void onAccuracyChanged(int sensor, int accuracy) {
 		
 		Log.i(TAG, "Sensor: [" + Integer.toString(sensor) + "]");
@@ -34,15 +31,13 @@ public class Cow extends Activity implements SensorListener {
 		accuracy_str.setText(Integer.toString(accuracy));
 	}
 
-	@Override
 	public void onSensorChanged(int sensor, float[] values) {
-		status.setText("Sensor: [" + Integer.toString(sensor) + "]");
+		int entry, allValues = values.length;
+		StringBuffer builtString = new StringBuffer(allValues);
 		
+		status.setText("Sensor: [" + Integer.toString(sensor) + "]");
 		Log.i(TAG, "Sensor: [" + Integer.toString(sensor) + "]");
 		
-    int entry, allValues = values.length;
-    StringBuffer builtString = new StringBuffer(allValues);
-
     for (entry = (allValues - 1); entry >= 0; entry--){
     	builtString.append(values[entry]);
     }
