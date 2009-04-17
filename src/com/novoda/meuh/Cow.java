8441c30800e9b9c7d8cc4a8541e61973176fc085
kevinmcdonagh <kevin@appletv.co.uk>
2009-04-17 19:58:54 +0100

    Added basic activity sensor

--- src/com/novoda/meuh/Cow.java
+++ src/com/novoda/meuh/Cow.java
@@ -1,15 +1,54 @@
 package com.novoda.meuh;
 
 import android.app.Activity;
+import android.hardware.SensorListener;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
-public class Cow extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+public class Cow extends Activity implements SensorListener {
+	
+	private static final String	TAG	= "[moo]:";
+	private TextView	status;
+	private TextView	coOrds;
+	private TextView	accuracy_str;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		status = (TextView) findViewById(R.id.str_status);
+		accuracy_str = (TextView) findViewById(R.id.str_accuracy);
+		coOrds = (TextView) findViewById(R.id.str_coords);
+
+	}
+
+	@Override
+	public void onAccuracyChanged(int sensor, int accuracy) {
+		
+		Log.i(TAG, "Sensor: [" + Integer.toString(sensor) + "]");
+		Log.i(TAG, "Accuracy:" + Integer.toString(accuracy) );
+		
+		status.setText("Sensor: [" + Integer.toString(sensor) + "]");
+		accuracy_str.setText(Integer.toString(accuracy));
+	}
+
+	@Override
+	public void onSensorChanged(int sensor, float[] values) {
+		status.setText("Sensor: [" + Integer.toString(sensor) + "]");
+		
+		Log.i(TAG, "Sensor: [" + Integer.toString(sensor) + "]");
+		
+    int entry, allValues = values.length;
+    StringBuffer builtString = new StringBuffer(allValues);
+
+    for (entry = (allValues - 1); entry >= 0; entry--){
+    	builtString.append(values[entry]);
     }
+		
+    Log.i(TAG, "CoOrds:" + builtString );
+		coOrds.setText(builtString);
+	}
+
 }
