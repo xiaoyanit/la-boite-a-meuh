f0e22f41db0cd6328bd786b1ff9f37c998bc0a52
kevinmcdonagh <kevin@appletv.co.uk>
2009-04-18 01:54:34 +0100

    Added the Moo states to build 'Moo power'. Upon starting the Application the cow enters into a dormant state. When the orientation dips under a certain point the cow builds moo power until returning back to the upstanding orientation. The cow then releases 'Moo power'.

--- src/com/novoda/meuh/MooService.java
+++ src/com/novoda/meuh/MooService.java
@@ -1,6 +1,8 @@
 package com.novoda.meuh;
 
 
+import java.util.Date;
+
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -17,7 +19,12 @@ public class MooService extends Service implements SensorListener {
   private NotificationManager mNM;
   private final IBinder mBinder = new LocalBinder();
   private SensorManager				mSensorManager;
+	private Date	startTime;
+	private int	cowState = DORMANT_COW;
+	private long	totalMooPower;
 	private static final String	TAG												= "[mooServ]:";
+	private static final int	DORMANT_COW	= 45;
+	private static final int	BUILDING_GAS	= 53;
   
   @Override
   public void onCreate() {
@@ -25,6 +32,9 @@ public class MooService extends Service implements SensorListener {
   		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
   		mSensorManager.registerListener(this, SensorManager.SENSOR_ACCELEROMETER | SensorManager.SENSOR_MAGNETIC_FIELD | SensorManager.SENSOR_ORIENTATION,
   				SensorManager.SENSOR_DELAY_FASTEST);
+  		
+  		this.startTime = new Date(000000000);
+  		
       showNotification();
   }
 
@@ -61,11 +71,20 @@ public class MooService extends Service implements SensorListener {
 	@Override
 	public void onSensorChanged(int sensor, float[] values) {
 		if (sensor == SensorManager.SENSOR_ORIENTATION) {
-			Log.d(TAG, "sensor: " + sensor + ", x: " + values[0] + ", y: " + values[1] + ", z: " + values[2]);		
+			//Log.d(TAG, "sensor: " + sensor + ", x: " + values[0] + ", y: " + values[1] + ", z: " + values[2]);		
+			
+			if(values[1] > -60 && cowState == DORMANT_COW){
+				startTime = new Date();
+				cowState = BUILDING_GAS;
+			}
+			
+			if(values[1] < -60 && cowState == BUILDING_GAS){
+				totalMooPower = startTime.getTime() - System.currentTimeMillis();
+				Log.d(TAG, "Moo Power: [" + totalMooPower + "]");
+				cowState = DORMANT_COW;
+			}
+			
+			
 		}
-		
-		
-		
-		
 	}
 }
