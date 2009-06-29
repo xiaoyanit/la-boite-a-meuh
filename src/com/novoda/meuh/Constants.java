9804718e360c32baddf02eb402a8bb6da4d697ce
kevinmcdonagh <kevin@appletv.co.uk>
2009-06-29 11:11:58 +0100

    Refactored a few lines into a Constants file.

---
+++ src/com/novoda/meuh/Constants.java
@@ -0,0 +1,10 @@
+package com.novoda.meuh;
+
+public class Constants {
+	public static final int			COLUMN_AUDIO_URI_ID				= 0;
+	public static final int			COLUMN_RELATIVE_FILE_LOCATION	= 1;
+	public static final int			COLUMN_FILENAME					= 2;
+	public static final int			COLUMN_INDEX4					= 2;
+	public static final int			COLUMN_INDEX5					= 2;
+	public static final String		AUDIO_FILES_DIR						= "/sdcard/la-boite-a-meuh/media/audio/meuhs";
+}
